c09981d575417934625ea544fdfa0a02f4e18ce7
김성훈 <ksnughnu@gmail.com>
2026-01-22 09:58:19 +0900

    오랜만에 푸니깐 쉽지않네

--- WEEK01/프로그래머스_숫자문자열과영단어/Oliver.swift
+++ WEEK01/프로그래머스_숫자문자열과영단어/Oliver.swift
@@ -1,1 +1,34 @@
+func solution(_ s: String) -> Int {
+    let trans: [String: String] = [
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    ]
 
+    var result = ""
+    var word = ""
+
+    for char in s {
+        // 숫자
+        if char >= "0" && char <= "9" {
+            result.append(char)
+        // 문자
+        } else {
+            word.append(char)
+        }
+
+        if let number = trans[word] {
+            result.append(number)
+            word = ""
+        }
+    }
+
+    return Int(result)!
+}
