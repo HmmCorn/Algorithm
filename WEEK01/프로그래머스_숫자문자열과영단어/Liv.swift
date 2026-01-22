af9831a8071a850ee3430accc37830d468b12173
Sang Yu Lee <sangyu857@gmail.com>
2026-01-22 10:18:51 +0900

    흠...

--- WEEK01/프로그래머스_숫자문자열과영단어/Liv.swift
+++ WEEK01/프로그래머스_숫자문자열과영단어/Liv.swift
@@ -1,1 +1,28 @@
+// 프로그래머스 - 숫자 문자열과 영단어
 
+import Foundation
+
+func solution(_ s:String) -> Int {
+    let numberMap: [String: Int] = [
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "zero": 0
+    ]
+
+    let numberStrings: [String] = numberMap.keys.map { $0 }.joined(separator: ",")
+    
+    let filtered = s.replacingOccurrences(
+        of: #"[numberStrings]+"#,
+        with: "5"
+    )
+    
+    let result: Int = Int(filtered) ?? 0
+    return result
+}
