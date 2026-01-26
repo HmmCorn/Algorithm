cf96ba106fb7d9973f69e982907d40c193f48c26
Sang Yu Lee <sangyu857@gmail.com>
2026-01-26 10:34:15 +0900

    zip 좀 가져다 썼읍니다

--- WEEK02/프로그래머스_비밀지도/Liv.swift
+++ WEEK02/프로그래머스_비밀지도/Liv.swift
@@ -6,16 +6,10 @@ func solution(_ n: Int, _ arr1: [Int], _ arr2: [Int]) -> [String] {
     let array2 = toMap(arr2, n)
     
     for (i, line) in array1.enumerated() {
-        var index = line.startIndex
         var str: String = ""
         
-        while index < line.endIndex {
-            if line[index] != array2[i][index] {
-                str.append("#")
-            } else {
-                str.append("line[index]")
-            }
-            index += 1
+        for (first, second) in zip(line, array2[i]) {
+            str.append(first == second ? (first == "1" ? "#" : " ") : "#")
         }
         
         answer.append(str)
@@ -27,11 +21,7 @@ func solution(_ n: Int, _ arr1: [Int], _ arr2: [Int]) -> [String] {
 func toMap(_ arr: [Int], _ count: Int) -> [String] {
     return arr.map {num in
         var str = String(num, radix: 2)
-        if str.count < count {
-            str = "0\(str)"
-        }
-        str = str.replacingOccurrences(of: "1", with: "#")
-        str = str.replacingOccurrences(of: "0", with: " ")
+        str = String(repeating: "0", count: count - str.count) + str
         return str
     }
 }
