b461c6df1dbccc007a8b4fdd765dcd281813814a
MinwooJe <59834326+MinwooJe@users.noreply.github.com>
2026-01-27 00:15:37 +0900

    성공) Swift 짱

--- WEEK02/프로그래머스_비밀지도/JSON.swift
+++ WEEK02/프로그래머스_비밀지도/JSON.swift
@@ -3,7 +3,7 @@
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     return zip(arr1, arr2).map {
         let row = String($0 | $1, radix: 2)
-        let formatted = String(format: "%0\(n)d", Int(row)!)
+        let formatted = String(repeating: "0", count: n - row.count) + row
         return formatted
             .map { $0 == "1" ? "#" : " " }
             .joined()
