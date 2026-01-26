f0850f726e9975ccdd4f21c597698a42d609c641
김성훈 <ksnughnu@gmail.com>
2026-01-26 14:21:49 +0900

    마늘맛
    - toBinaryArray 수정

--- WEEK02/프로그래머스_비밀지도/Oliver.swift
+++ WEEK02/프로그래머스_비밀지도/Oliver.swift
@@ -1,17 +1,17 @@
 func solution(_ n: Int, _ arr1: [Int], _ arr2: [Int]) -> [String] {
 
-    // 10진수 -> 2진수 string 타입 함수 정의
-    func toBinaryString(_ num: Int) -> [Character] {
+    // 10진수 -> 2진수 함수 정의
+    func toBinaryArray(_ num: Int) -> [Character] {
         let binary = String(num, radix: 2)
-        // 맨 앞자리가 0일때 처리;;
-        return binary
+        let padded = String(repeating: "0", count: n - binary.count) + binary
+        return Array(padded)
     }
 
     var result: [String] = []
 
     for i in 0..<n {
-        let binaryArr1 = toBinaryString(arr1[i])
-        let binaryArr2 = toBinaryString(arr2[i])
+        let binaryArr1 = toBinaryArray(arr1[i])
+        let binaryArr2 = toBinaryArray(arr2[i])
 
         var line = ""
 
