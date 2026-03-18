60b9bb389a8e593ce9929580e486ff42e25df515
00yhsp <00yhsp@naver.com>
2026-03-19 01:21:35 +0900

    입국심사

--- WEEK07/프로그래머스_입국심사/aiden.swift
+++ WEEK07/프로그래머스_입국심사/aiden.swift
@@ -1,34 +1,22 @@
-let nc = readLine()!.split(separator: " ").compactMap { Int($0) }
-let (n, c) = (nc[0], nc[1])
+import Foundation
 
-var arr = [Int]()
-for _ in 0..<n { arr.append(Int(readLine()!)!) }
-arr.sort()
-
-var start = 0; var end = 1_000_000_000
-
-while start <= end {
-    let mid = (start + end) / 2
-
-    if promising(mid) {
-        start = mid + 1
-    } else {
-        end = mid - 1
-    }
-}
-
-print(end)
-
-func promising(_ distance: Int) -> Bool {
-    var count = 0
-    var prev = 0
+func solution(_ n:Int, _ times:[Int]) -> Int64 {
+    var start = 1
+    var end = 1_000_000_000_000_000_000
     
-    for i in 0..<n {
-        if arr[i] - prev >= distance || i == 0 {
-            count += 1
-            prev = arr[i]
+    while start <= end {
+        let mid = (start + end) / 2
+        var count = 0
+        for time in times {
+            count += mid / time
+        }
+        if count < n {
+            start = mid + 1    
+        } else {
+            end = mid - 1
         }
     }
+    
+    return start
+}
 
-    return count >= c
-} 
